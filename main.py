5a8636188cde8097ff63d2bab052d67c40434e5d
simicr <simradovan@gmail.com>
2023-12-10 22:56:41 +0100

    small changes, added 2 arch

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pickle
 
 N_CLASSES = 20
 INPUT_SIZE = (32, 32, 1)
-LOSS = tf.losses.CategoricalCrossentropy(from_logits=True)
+LOSS = 'categorical_crossentropy'
 METRICS = ['accuracy']
 CALLBACKS = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)] # Correct?
 BATCH_SIZE = 32
@@ -26,38 +26,68 @@ y_test = tf.keras.utils.to_categorical(y_test, num_classes=N_CLASSES)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, stratify=y_train, random_state=42)
 
 TRAIN_SIZE, _ , _ , _ = x_train.shape
-INPUT_IMG = tf.keras.Input(shape=INPUT_SIZE)
+input_img = tf.keras.Input(shape=INPUT_SIZE)
 y_pred = None
 
-
-
+def task1():
+    fig, axs = plt.subplots(4, 5)
+    axs = axs.flatten()
+    for i, ax in enumerate(axs):
+        ax.imshow(x_train[i])
+        print(y_train[i])
+        ax.set_xticks([])
+        ax.set_yticks([])
+    plt.show()
 
 
 def M1():
-    # CNN from the practicals, only for testing if the code works.
     global y_pred
 
-    h1 = tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation="relu", padding="same")(INPUT_IMG)
+    h1 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='same')(input_img)
     h1 = tf.keras.layers.MaxPooling2D((2, 2))(h1)
 
-    h2 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu", padding="valid")(h1)
+    h2 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='valid')(h1)
     h2 = tf.keras.layers.MaxPooling2D((2, 2))(h2)
 
-    h3 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu", padding="valid")(h2)
+    h3 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='same')(h2)
     h3 = tf.keras.layers.MaxPooling2D((2, 2))(h3)
 
-    h4 = tf.keras.layers.Flatten()(h3)
-    h5 = tf.keras.layers.Dense(units=100, activation="relu")(h4)
-    h6 = tf.keras.layers.Dense(units=20)(h5)
-    
-    y_pred = tf.keras.layers.Dense(units=N_CLASSES)(h6)
+    fh1 = tf.keras.layers.Flatten()(h3)
+    fh2 = tf.keras.layers.Dense(units=128, activation='relu')(fh1)
+    fh3 = tf.keras.layers.Dense(units=32, activation='relu')(fh2)
+    y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh3)
+
+def M2():
+    global y_pred
+
+    h1 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3,3), activation='relu', padding='same')(input_img)
+    h1 = tf.keras.layers.MaxPool2D((2,2))(h1)
+
+    h2 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='same')(h1)
+    h2 = tf.keras.layers.MaxPooling2D((2, 2))(h2)
+
+    h3 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='same')(h2)
+    h3 = tf.keras.layers.MaxPooling2D((2, 2))(h3)
+
+    h4 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3,3), activation='relu', padding='same')(h3)
+    h4 = tf.keras.layers.MaxPool2D((2,2))(h3)
+
+    fh1 = tf.keras.layers.Flatten()(h4)
+    fh2 = tf.keras.layers.Dense(units=128, activation='relu')(fh1)
+    fh3 = tf.keras.layers.Dense(units=128, activation='relu')(fh2)
+    fh4 = tf.keras.layers.Dense(units=32, activation='relu')(fh3)
+    y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh4)
+
+def M3():
+    global y_pred
 
 
-M1()
-model = tf.keras.Model(INPUT_IMG, y_pred)
+
+
+M2()
+model = tf.keras.Model(input_img, y_pred)
 model.summary()
 
-# Data augmentation prob, needs changing, this is taken from the practicals. 
 sampler = tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True, vertical_flip=True,
                                                           width_shift_range=0.1, height_shift_range=0.1).flow(x_train, y_train, batch_size=BATCH_SIZE)
 
