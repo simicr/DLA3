3a131774b9b0f64de327b523051df87612322b25
simicr <simradovan@gmail.com>
2023-12-16 11:58:08 +0100

    Task B

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ N_CLASSES = 20
 INPUT_SIZE = (32, 32, 1)
 LOSS = 'categorical_crossentropy'
 METRICS = ['accuracy']
-CALLBACKS = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3), tf.keras.callbacks.LearningRateScheduler(scheduler)] # Correct?
+CALLBACKS = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True), tf.keras.callbacks.LearningRateScheduler(scheduler)]
 BATCH_SIZE = 32
-EPOCHS = 15
+EPOCHS = 20
 
 train_data, test_data = tfd.cifar100.load_data(label_mode="coarse")
 (x_train, y_train), (x_test, y_test) = train_data, test_data
@@ -31,19 +31,19 @@ y_test = tf.keras.utils.to_categorical(y_test, num_classes=N_CLASSES)
 
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, stratify=y_train, random_state=42)
 
-# normalize input data
-x_train_n = (x_train.astype('float32')) / 255
-x_val_n = (x_val.astype('float32')) / 255
+#normalize input data
+#x_train_n = (x_train.astype('float32')) / 255
+#x_val_n = (x_val.astype('float32')) / 255
 
 TRAIN_SIZE, _ , _ , _ = x_train.shape
 input_img = tf.keras.Input(shape=INPUT_SIZE)
 y_pred = None
 
 
-
-
-
-def task1():
+#
+# TASKA A
+#
+def task_a():
     fig, axs = plt.subplots(4, 5)
     axs = axs.flatten()
     for i, ax in enumerate(axs):
@@ -54,16 +54,20 @@ def task1():
     plt.show()
 
 
+#
+# TASK B
+#
+
 def M1():
     global y_pred
 
     h1 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='same')(input_img)
     h1 = tf.keras.layers.MaxPooling2D((2, 2))(h1)
 
-    h2 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='valid')(h1)
+    h2 = tf.keras.layers.Conv2D(filters=8, kernel_size=(3, 3), activation='relu', padding='valid')(h1)
     h2 = tf.keras.layers.MaxPooling2D((2, 2))(h2)
 
-    h3 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='same')(h2)
+    h3 = tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(h2)
     h3 = tf.keras.layers.MaxPooling2D((2, 2))(h3)
 
     fh1 = tf.keras.layers.Flatten()(h3)
@@ -75,18 +79,18 @@ def M2():
     global y_pred
 
     h0 = tf.keras.layers.Conv2D(filters=4, kernel_size=(1,1))(input_img)
-    # h0 = tf.keras.layers.MaxPool2D((1,1))(h0) #this does nothing
+    h0 = tf.keras.layers.MaxPool2D((1,1))(h0)
 
-    h1 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3,3), activation='relu', padding='same')(h0)
+    h1 = tf.keras.layers.Conv2D(filters=8, kernel_size=(3,3), activation='relu', padding='same')(h0)
     h1 = tf.keras.layers.MaxPool2D((2,2))(h1)
 
-    h2 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='same')(h1)
+    h2 = tf.keras.layers.Conv2D(filters=8, kernel_size=(3, 3), activation='relu', padding='same')(h1)
     h2 = tf.keras.layers.MaxPooling2D((2, 2))(h2)
 
-    h3 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3, 3), activation='relu', padding='same')(h2)
+    h3 = tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(h2)
     h3 = tf.keras.layers.MaxPooling2D((2, 2))(h3)
 
-    h4 = tf.keras.layers.Conv2D(filters=4, kernel_size=(3,3), activation='relu', padding='same')(h3)
+    h4 = tf.keras.layers.Conv2D(filters=16, kernel_size=(3,3), activation='relu', padding='same')(h3)
     h4 = tf.keras.layers.MaxPool2D((2,2))(h3)
 
     fh1 = tf.keras.layers.Flatten()(h4)
@@ -98,19 +102,19 @@ def M2():
 def M3():
     global y_pred
 
-    h0 = tf.keras.layers.Conv2D(filters=3, kernel_size=(1,1))(input_img)
-    h0 = tf.keras.layers.MaxPool2D((1,1))(h0) #this does nothing
+    h0 = tf.keras.layers.Conv2D(filters=8, kernel_size=(1,1))(input_img)
+    h0 = tf.keras.layers.MaxPool2D((1,1))(h0) 
 
-    h1 = tf.keras.layers.Conv2D(filters=3, kernel_size=(5,5), padding='same')(h0)
+    h1 = tf.keras.layers.Conv2D(filters=8, kernel_size=(5,5), padding='same')(h0)
     h1 = tf.keras.layers.MaxPool2D((2,2))(h1)
 
-    h2 = tf.keras.layers.Conv2D(filters=5, kernel_size=(3,3), padding='same')(h1)
+    h2 = tf.keras.layers.Conv2D(filters=8, kernel_size=(3,3), padding='same')(h1)
     h2 = tf.keras.layers.MaxPool2D((2,2))(h2)
 
-    h3 = tf.keras.layers.Conv2D(filters=3, kernel_size=(3,3), padding='same')(h2)
+    h3 = tf.keras.layers.Conv2D(filters=8, kernel_size=(3,3), padding='same')(h2)
     h3 = tf.keras.layers.MaxPool2D((2,2))(h3)
 
-    h4 = tf.keras.layers.Conv2D(filters=3, kernel_size=(3,3), padding='same')(h3)
+    h4 = tf.keras.layers.Conv2D(filters=8, kernel_size=(3,3), padding='same')(h3)
     h4 = tf.keras.layers.MaxPool2D((2,2))(h4)
 
     fh1 = tf.keras.layers.Flatten()(h4)
@@ -119,9 +123,29 @@ def M3():
     y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh3)
 
 
+def M4():
+    global y_pred
+
+    h0 = tf.keras.layers.Conv2D(filters=16, kernel_size=(5,5))(input_img)
+    
+ 
+    h1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3), padding='same')(h0)
+    h1 = tf.keras.layers.AvgPool2D((3,3))(h1)
+
+    h2 = tf.keras.layers.Conv2D(filters=64, kernel_size=(3,3), padding='same')(h1)
+    h2 = tf.keras.layers.MaxPool2D((2,2))(h2)
+
+    h3 = tf.keras.layers.Conv2D(filters=16, kernel_size=(3,3), padding='same')(h2)
+    h3 = tf.keras.layers.MaxPool2D((2,2))(h3)
+
+
+    fh1 = tf.keras.layers.Flatten()(h3)
+    fh2 = tf.keras.layers.Dense(units=128, activation='relu')(fh1)
+    fh3 = tf.keras.layers.Dense(units=64, activation='relu')(fh2)
+    y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh3)
+
 def M5():
     global y_pred
-    #double number of filters on each iteration - generic first layers, specific later layers 
     h0 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3))(input_img)
     h0 = tf.keras.layers.MaxPool2D((2,2))(h0)
 
@@ -138,29 +162,36 @@ def M5():
     fh3 = tf.keras.layers.Dense(units=128, activation='relu')(fh2)
     y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh3)
 
+#
+# TASK C
+#
 
-
-
-M5()
-#added model name to save statistics and keep track of it at least a bit - convenient for converting to latex later too - can delete for submission
-model_name = 'M5'
+#
+# TASK D
+#
+    
+#
+# TASK E
+#
+    
+M3()
+model_name = 'M3'
 model = tf.keras.Model(input_img, y_pred)
 model.summary()
 
-sampler = tf.keras.preprocessing.image.ImageDataGenerator(horizontal_flip=True, vertical_flip=True,
-                                                          width_shift_range=0.1, height_shift_range=0.1).flow(x_train_n, y_train, batch_size=BATCH_SIZE)
-
-
+sampler = tf.keras.preprocessing.image.ImageDataGenerator().flow(x_train, y_train, batch_size=BATCH_SIZE)
 optimizer = tf.keras.optimizers.Adam()
 
 model.compile(optimizer=optimizer, loss=LOSS, metrics=METRICS)
-history = model.fit(sampler, epochs=EPOCHS, steps_per_epoch=TRAIN_SIZE//BATCH_SIZE, callbacks=CALLBACKS, validation_data=(x_val_n, y_val))
+history = model.fit(sampler, epochs=EPOCHS, steps_per_epoch=TRAIN_SIZE//BATCH_SIZE, callbacks=CALLBACKS, validation_data=(x_val, y_val))
+
+
 
 #csv QoL
 history_df = pd.DataFrame(history.history)
 history_df.to_csv(f'{model_name}_training_history.csv', index=False)
 
-test_loss, test_acc = model.evaluate(x_val_n, y_val)
+test_loss, test_acc = model.evaluate(x_val, y_val)
 
 #csv QoL
 evaluation_df = pd.DataFrame({'Test Loss': [test_loss], 'Test Accuracy': [test_acc]})
