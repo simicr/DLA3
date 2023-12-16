362d19f0b152752d5f28401160d6b43346b62013
simicr <simradovan@gmail.com>
2023-12-16 17:39:46 +0100

    Task C: dropout and reg

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from keras import datasets as tfd
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import pickle
-import pandas as pd #for saving results
 
 def scheduler(epoch, lr):
    if epoch < 10:
@@ -13,6 +12,7 @@ def scheduler(epoch, lr):
    else:
      return lr * tf.math.exp(-0.1)
 
+# Define constants
 N_CLASSES = 20
 INPUT_SIZE = (32, 32, 1)
 LOSS = 'categorical_crossentropy'
@@ -21,6 +21,7 @@ CALLBACKS = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, re
 BATCH_SIZE = 32
 EPOCHS = 20
 
+# Preparing the data
 train_data, test_data = tfd.cifar100.load_data(label_mode="coarse")
 (x_train, y_train), (x_test, y_test) = train_data, test_data
 x_train, x_test = np.mean(x_train, axis=3), np.mean(x_test, axis=3)
@@ -31,10 +32,8 @@ y_test = tf.keras.utils.to_categorical(y_test, num_classes=N_CLASSES)
 
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, stratify=y_train, random_state=42)
 
-#normalize input data
-#x_train_n = (x_train.astype('float32')) / 255
-#x_val_n = (x_val.astype('float32')) / 255
 
+# Define some more variables.
 TRAIN_SIZE, _ , _ , _ = x_train.shape
 input_img = tf.keras.Input(shape=INPUT_SIZE)
 y_pred = None
@@ -127,7 +126,6 @@ def M4():
     global y_pred
 
     h0 = tf.keras.layers.Conv2D(filters=16, kernel_size=(5,5))(input_img)
-    
  
     h1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3), padding='same')(h0)
     h1 = tf.keras.layers.AvgPool2D((3,3))(h1)
@@ -137,7 +135,6 @@ def M4():
 
     h3 = tf.keras.layers.Conv2D(filters=16, kernel_size=(3,3), padding='same')(h2)
     h3 = tf.keras.layers.MaxPool2D((2,2))(h3)
-
 
     fh1 = tf.keras.layers.Flatten()(h3)
     fh2 = tf.keras.layers.Dense(units=128, activation='relu')(fh1)
@@ -155,8 +152,6 @@ def M5():
     h2 = tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), padding='same')(h1)
     h2 = tf.keras.layers.MaxPool2D((2,2))(h2)
 
-
-
     fh1 = tf.keras.layers.Flatten()(h2)
     fh2 = tf.keras.layers.Dense(units=256, activation='relu')(fh1)
     fh3 = tf.keras.layers.Dense(units=128, activation='relu')(fh2)
@@ -165,35 +160,117 @@ def M5():
 #
 # TASK C
 #
-
-#
-# TASK D
-#
     
+# Adding L2 reg with lambda=0.1 on the FC part
+def C1():
+    global y_pred
+    h0 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3))(input_img)
+    h0 = tf.keras.layers.MaxPool2D((2,2))(h0)
+
+    h1 = tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), padding='same')(h0)
+    h1 = tf.keras.layers.MaxPool2D((2,2))(h1)
+
+    h2 = tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), padding='same')(h1)
+    h2 = tf.keras.layers.MaxPool2D((2,2))(h2)
+
+    fh1 = tf.keras.layers.Flatten()(h2)
+    fh2 = tf.keras.layers.Dense(units=256, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01))(fh1)
+    fh3 = tf.keras.layers.Dense(units=128, activation='relu',  kernel_regularizer=tf.keras.regularizers.L2(0.01))(fh2)
+    y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh3)
+
+# Adding L2 reg with lambda=0.1 on the FC part
+def C2():
+    global y_pred
+    h0 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3))(input_img)
+    h0 = tf.keras.layers.MaxPool2D((2,2))(h0)
+
+    h1 = tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), padding='same')(h0)
+    h1 = tf.keras.layers.MaxPool2D((2,2))(h1)
+
+    h2 = tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), padding='same')(h1)
+    h2 = tf.keras.layers.MaxPool2D((2,2))(h2)
+
+    fh1 = tf.keras.layers.Flatten()(h2)
+    fh2 = tf.keras.layers.Dense(units=256, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.1))(fh1)
+    fh3 = tf.keras.layers.Dense(units=128, activation='relu',  kernel_regularizer=tf.keras.regularizers.L2(0.1))(fh2)
+    y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh3)
+
+# Adding L2 reg with lambda=0.001 in the FC part
+def C3():
+    global y_pred
+    h0 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3))(input_img)
+    h0 = tf.keras.layers.MaxPool2D((2,2))(h0)
+
+    h1 = tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), padding='same')(h0)
+    h1 = tf.keras.layers.MaxPool2D((2,2))(h1)
+
+    h2 = tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), padding='same')(h1)
+    h2 = tf.keras.layers.MaxPool2D((2,2))(h2)
+
+    fh1 = tf.keras.layers.Flatten()(h2)
+    fh2 = tf.keras.layers.Dense(units=256, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001))(fh1)
+    fh3 = tf.keras.layers.Dense(units=128, activation='relu',  kernel_regularizer=tf.keras.regularizers.L2(0.001))(fh2)
+    y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh3)
+
+# Adding dropout with rate 0.5 in the FC part.
+def C4():
+    global y_pred
+    global y_pred
+    h0 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3))(input_img)
+    h0 = tf.keras.layers.MaxPool2D((2,2))(h0)
+
+    h1 = tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), padding='same')(h0)
+    h1 = tf.keras.layers.MaxPool2D((2,2))(h1)
+
+    h2 = tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), padding='same')(h1)
+    h2 = tf.keras.layers.MaxPool2D((2,2))(h2)
+
+    fh1 = tf.keras.layers.Flatten()(h2)
+    fh2 = tf.keras.layers.Dense(units=256, activation='relu')(fh1)
+    fh2 = tf.keras.layers.Dropout(0.5)(fh2)
+    fh3 = tf.keras.layers.Dense(units=128, activation='relu')(fh2)
+    fh3 = tf.keras.layers.Dropout(0.5)(fh3)
+    y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh3)
+
+# Adding dropout with rate 0.1 in the FC part.
+def C5():
+    global y_pred
+    global y_pred
+    h0 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3))(input_img)
+    h0 = tf.keras.layers.MaxPool2D((2,2))(h0)
+
+    h1 = tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), padding='same')(h0)
+    h1 = tf.keras.layers.MaxPool2D((2,2))(h1)
+
+    h2 = tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), padding='same')(h1)
+    h2 = tf.keras.layers.MaxPool2D((2,2))(h2)
+
+    fh1 = tf.keras.layers.Flatten()(h2)
+    fh2 = tf.keras.layers.Dense(units=256, activation='relu')(fh1)
+    fh2 = tf.keras.layers.Dropout(0.1)(fh2)
+    fh3 = tf.keras.layers.Dense(units=128, activation='relu')(fh2)
+    fh3 = tf.keras.layers.Dropout(0.1)(fh3)
+    y_pred = tf.keras.layers.Dense(units=N_CLASSES, activation='softmax')(fh3)
 #
 # TASK E
 #
-    
-M3()
-model_name = 'M3'
+
+sampler = tf.keras.preprocessing.image.ImageDataGenerator().flow(x_train, y_train, batch_size=BATCH_SIZE)
+optimizer = tf.keras.optimizers.Adam()    
+C5()
+model_name = 'C5'
+
 model = tf.keras.Model(input_img, y_pred)
 model.summary()
 
-sampler = tf.keras.preprocessing.image.ImageDataGenerator().flow(x_train, y_train, batch_size=BATCH_SIZE)
-optimizer = tf.keras.optimizers.Adam()
 
 model.compile(optimizer=optimizer, loss=LOSS, metrics=METRICS)
 history = model.fit(sampler, epochs=EPOCHS, steps_per_epoch=TRAIN_SIZE//BATCH_SIZE, callbacks=CALLBACKS, validation_data=(x_val, y_val))
-
-
-
-#csv QoL
-history_df = pd.DataFrame(history.history)
-history_df.to_csv(f'{model_name}_training_history.csv', index=False)
-
 test_loss, test_acc = model.evaluate(x_val, y_val)
 
-#csv QoL
+# Write down results in a csv file.
+history_df = pd.DataFrame(history.history)
+history_df.to_csv(f'{model_name}_training_history.csv', index=False)
 evaluation_df = pd.DataFrame({'Test Loss': [test_loss], 'Test Accuracy': [test_acc]})
 evaluation_df.to_csv(f'{model_name}_evaluation_results.csv', index=False)
 
